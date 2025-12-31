5fec09704a91e99f3c413a5752b462065f42c60f
hbkim404 <rubikkaki@gmail.com>
2025-12-31 00:37:09 -0300

    optimize recipe search process  when data is unchanged

--- main.py
+++ main.py
@@ -2,7 +2,12 @@ from scrapy.crawler import CrawlerProcess
 from scrapy.utils.project import get_project_settings
 from pizza_recipes.pizza_recipes.spiders.recipes import RecipesSpider
 
+from pathlib import Path
+
 from utils.find_recipe import find_recipe
+
+indexed_file_name = "indexed_pizza_recipes.json"
+indexed_file_dir = Path(indexed_file_name)
 
 
 def scrap_recipes():
@@ -26,12 +31,26 @@ def scrap_recipes():
     process.start()
 
 
+def delete_existing_indexed_recipes(file_dir: Path) -> None:
+    try:
+        file_dir.unlink(missing_ok=True)
+        print(f"File '{file_dir}' has been deleted (if it existed).")
+
+    except PermissionError:
+        print(f"Permission denied to delete the file '{file_dir}'.")
+
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+
 if __name__ == "__main__":
     scrap_recipes()
+    delete_existing_indexed_recipes(indexed_file_dir)
 
     print("*" * 20)
 
-    recipe = find_recipe("Pizza keto de pollo")
+    find_recipe("Pizza keto de pollo")
+    find_recipe("Nombre inexistente")
+    find_recipe("Masa de pizza de coliflor")
 
-    print(f"Recipe found: {recipe}") if recipe else print("No recipe found")
     print("*" * 20)
--- utils/find_recipe.py
+++ utils/find_recipe.py
@@ -1,22 +1,72 @@
 import json
+import functools
 
-from collections import defaultdict
 from pathlib import Path
-from typing import Optional
+from collections import defaultdict
+
+original_file_name = "pizza_recipes.json"
+indexed_file_name = "indexed_pizza_recipes.json"
 
 
-def get_recipes_directory():
+def safe_file_open(func):
+    @functools.wraps(func)
+    def wrapper(file_path, mode, *args, **kwargs):
+        try:
+            with open(file_path, mode) as f:
+                return func(f, *args, **kwargs)
+
+        except FileNotFoundError:
+            print("File not found!")
+
+        except TypeError:
+            print("Error: Failed to encode JSON to the file")
+
+        except json.JSONDecodeError:
+            print("Error: Failed to decode JSON from the file.")
+
+        except Exception:
+            print("Recipe not found.")
+
+    return wrapper
+
+
+@safe_file_open
+def retrieve_recipes(f):
+    data = json.load(f)
+    indexed_recipes = index_recipes(data)
+
+    return indexed_recipes
+
+
+@safe_file_open
+def generate_indexed_recipes(f, data: defaultdict):
+    json.dump(data, f, ensure_ascii=False, indent=4)
+
+
+@safe_file_open
+def retrieve_indexed_recipe(f, name: str):
+    data = json.load(f)
+    recipe = data[name]
+
+    return recipe if recipe else "Recipe not found"
+
+
+def locate_indexed_file(file_dir: Path) -> bool:
+    file_path = Path(file_dir)
+    return True if file_path.is_file() else False
+
+
+def get_file_directory(file_name: str):
     # utilized in case the script is run directly from its file
     script_parent_dir = Path(__file__).resolve().parent
 
     project_root = script_parent_dir.parent
-    recipes_dir = project_root / "pizza_recipes.json"
+    file_dir = project_root / file_name
 
-    return recipes_dir
+    return file_dir
 
 
 def index_recipes(recipe_array: list) -> defaultdict:
-    # utilizing defaultdict in case a given key doesn't exist
     index = defaultdict(list)
 
     for recipe in recipe_array:
@@ -27,26 +77,35 @@ def index_recipes(recipe_array: list) -> defaultdict:
     return index
 
 
-def find_recipe(name: str) -> Optional[dict]:
-    try:
-        with open(get_recipes_directory(), "r") as f:
-            data = json.load(f)
+def find_recipe(recipe_name: str):
+    original_file_dir = get_file_directory(original_file_name)
+    indexed_file_dir = get_file_directory(indexed_file_name)
 
-            indexed_recipes = index_recipes(data)
-            recipe = indexed_recipes[name]
+    original_file_exists = locate_indexed_file(original_file_dir)
+    indexed_file_exists = locate_indexed_file(indexed_file_dir)
 
-            return recipe if recipe else None
+    recipe = None
 
-    except FileNotFoundError:
-        print("File not found!")
+    print("Retrieving recipe...")
 
-    except json.JSONDecodeError:
-        print("Error: Failed to decode JSON from the file.")
+    if indexed_file_exists:
+        recipe = retrieve_indexed_recipe(indexed_file_dir, mode="r", name=recipe_name)
+    elif original_file_exists:
+        data = retrieve_recipes(original_file_dir, mode="r")
+
+        if type(data) is defaultdict:
+            generate_indexed_recipes(indexed_file_dir, mode="w", data=data)
+            recipe = retrieve_indexed_recipe(
+                indexed_file_dir, mode="r", name=recipe_name
+            )
+    else:
+        print("No recipe files found.")
+
+    if recipe:
+        print(f"Recipe found: {recipe}")
 
 
 if __name__ == "__main__":
-    recipe = find_recipe("Pizza keto de pollo")
-    recipe2 = find_recipe("Pizza no existente")
-
-    print(recipe)
-    print(recipe2)
+    find_recipe("Pizza keto de pollo")
+    find_recipe("Nombre inexistente")
+    find_recipe("Masa de pizza de coliflor")
